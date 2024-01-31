bb5121b35baea4db538655ce4e70522dea3ed737
RuteSantos <1160663@isep.ipp.pt>
2024-01-31 20:30:26 +0000

    fix: validate if encrypted contacts list is in base64 format

--- src/data/transform.test.ts
+++ src/data/transform.test.ts
@@ -4,7 +4,7 @@ import { requestToEncryptedContactsList } from './transform';
 
 describe('requestToEncryptedContactsList', () => {
 	test('should return EncryptedContactsList with correct contacts', () => {
-		const contactsValue = 'encryptedContactString';
+		const contactsValue = 'U29tZVN0cmluZ09idmlvdXNseU5vdEJhc2U2NEVuY29kZWQ=';
 		const req: any = {
 			body: {
 				contacts: contactsValue
@@ -18,15 +18,26 @@ describe('requestToEncryptedContactsList', () => {
 		});
 	});
 
+	test('should throw exception when contacts is not in base 64', () => {
+		const contactsValue = 'encryptedContactString';
+		const req: any = {
+			body: {
+				contacts: contactsValue
+			}
+		};
+
+		expect(() => requestToEncryptedContactsList(req)).toThrow(
+			'encrypted contacts list is not in base64 format'
+		);
+	});
+
 	test('should return EncryptedContactsList with empty contacts if body is empty', () => {
 		const req: any = {
 			body: {}
 		};
 
-		const result: EncryptedContactsList = requestToEncryptedContactsList(req);
-
-		expect(result).toEqual({
-			contacts: undefined
-		});
+		expect(() => requestToEncryptedContactsList(req)).toThrowError(
+			'encrypted contacts list is not in base64 format'
+		);
 	});
 });
--- src/data/transform.ts
+++ src/data/transform.ts
@@ -1,11 +1,18 @@
 import { EncryptedContactsList } from './model';
 
+const base64regex =
+	/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
 export function requestToEncryptedContactsList(
 	req: Request
 ): EncryptedContactsList {
 	const requestBody: any = req.body;
 
-	return <EncryptedContactsList>{
-		contacts: requestBody.contacts
-	};
+	if (base64regex.test(requestBody.contacts)) {
+		return <EncryptedContactsList>{
+			contacts: requestBody.contacts
+		};
+	}
+
+	throw 'encrypted contacts list is not in base64 format';
 }
