17f4fc6ce37df5414b0c02926f538b51f3689609
RuteSantos <1160663@isep.ipp.pt>
2024-01-31 21:54:38 +0000

    refactor: removed unnecessary escape character: \/

--- src/http/param.ts
+++ src/http/param.ts
@@ -1,7 +1,7 @@
 import { DocumentReference } from '../data';
 import { contactsRoute } from './route';
 
-const refPathParameterRegex = new RegExp(`\/${contactsRoute}([^\/]+)`);
+const refPathParameterRegex = new RegExp(`${contactsRoute}([^/]+)`);
 
 export function extractDocRef(url: URL): DocumentReference | undefined {
 	return url.pathname.match(refPathParameterRegex)?.at(1);
