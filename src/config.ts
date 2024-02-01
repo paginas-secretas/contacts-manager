a460fbdc02038ba1d6f2d3b30f2de57b1b024847
freitzzz <1160907@isep.ipp.pt>
2024-02-01 09:15:27 +0000

    chore(env): specific required environment config to connect to contacts manager db

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,28 @@
 export interface Env {
-	AWESOME_SECRET: string;
+	CONTACTS_DB_GITHUB_REPOSITORY_OWNER: string;
+	CONTACTS_DB_GITHUB_REPOSITORY_REPO: string;
+	CONTACTS_DB_GITHUB_REPOSITORY_BRANCH: string;
+	CONTACTS_DB_GITHUB_REPOSITORY_TOKEN: string;
+}
+
+interface ContactsManagerDBGitHubConfig {
+	owner: string;
+	repo: string;
+	branch: string;
+	token: string;
 }
 
 export interface Config {
-	awesomeSecret: string;
+	contactsManagerDB: ContactsManagerDBGitHubConfig;
 }
 
 export function fromEnv(env: Env): Config {
 	return {
-		awesomeSecret: env.AWESOME_SECRET
+		contactsManagerDB: {
+			branch: env.CONTACTS_DB_GITHUB_REPOSITORY_BRANCH,
+			owner: env.CONTACTS_DB_GITHUB_REPOSITORY_OWNER,
+			repo: env.CONTACTS_DB_GITHUB_REPOSITORY_REPO,
+			token: env.CONTACTS_DB_GITHUB_REPOSITORY_TOKEN,
+		}
 	};
 }
