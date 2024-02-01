ae0df2308eef8745caf3e2c8487a980b5d4e68a9
freitzzz <1160907@isep.ipp.pt>
2024-02-01 20:53:06 +0000

    docs(route): add ts-docs

--- src/http/route.ts
+++ src/http/route.ts
@@ -2,11 +2,22 @@ export const contactsRoute = '/contacts/';
 
 const routeRegex = buildRouteRegex(['contacts']);
 
+/**
+ * Types supported routes.
+ */
 interface RoutingTable {
-	contacts: () => Response;
+	contacts: () => Promise<Response>;
 	default: () => Response;
 }
 
+/**
+ * Handles a request by calling the appropriate function that matches the route for such request.
+ * Use this function as the entry call when a new request is received.
+ *
+ * @param request - the incoming request to be handled
+ * @param routing - the routing table that provides the outgoing responses each route
+ * @returns the response returned by the matched routing callback.
+ */
 export function handleRequestRouting(request: Request, routing: RoutingTable) {
 	const endpoint = new URL(request.url).pathname;
 	const matches = endpoint.match(routeRegex)?.filter((x) => x != undefined);
@@ -20,6 +31,18 @@ export function handleRequestRouting(request: Request, routing: RoutingTable) {
 	}
 }
 
+/**
+ * Builds a regular expression that is able to match REST routes (based on collections) through group values.
+ *
+ * Example: to match collection "foo" and resources associated to it, the function will return the following expression:
+ *
+ * `^/(foo)/?([^/\n]*)?/?$`
+ *
+ * The first group value matches "foo" and the second group the collection resources.
+ *
+ * @param collections
+ * @returns a regular expression able to match collections and sub-collections.
+ */
 export function buildRouteRegex(collections: string[]): RegExp {
 	return new RegExp(
 		`^/${collections
