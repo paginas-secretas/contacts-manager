4a8816ba3a4a9af29ae61c443a7b85975850a02c
freitzzz <1160907@isep.ipp.pt>
2024-02-01 15:39:41 +0000

    feat(http): initial draft implementation of request routing

--- src/http/route.ts
+++ src/http/route.ts
@@ -1,1 +1,32 @@
-export const contactsRoute = 'contacts/';
+export const contactsRoute = '/contacts/';
+
+interface RoutingTable {
+    contacts: () => Response,
+    default: () => Response
+};
+
+export function handleRequestRouting(
+    request: Request,
+    routing: RoutingTable
+) {
+    const endpoint = extractEndpointFromRequest(request);
+
+    switch (endpoint) {
+        case contactsRoute:
+            return routing.contacts();
+        default:
+            return routing.default();
+    }
+};
+
+function extractEndpointFromRequest(
+    request: Request
+) {
+    let endpoint = new URL(request.url).pathname;
+
+    if (!endpoint.endsWith('/')) {
+        endpoint = `${endpoint}/`
+    }
+
+    return endpoint;
+}
