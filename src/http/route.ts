27f681833beb729c531a142b4142f6998a615e2c
freitzzz <1160907@isep.ipp.pt>
2024-02-01 19:55:19 +0000

    refactor(route): fix handleRequestRouting not detecting references of contacts collection

--- src/http/route.ts
+++ src/http/route.ts
@@ -1,27 +1,27 @@
 export const contactsRoute = '/contacts/';
 
+const routeRegex = buildRouteRegex(['contacts']);
+
 interface RoutingTable {
 	contacts: () => Response;
 	default: () => Response;
 }
 
 export function handleRequestRouting(request: Request, routing: RoutingTable) {
-	const endpoint = extractEndpointFromRequest(request);
+	const endpoint = new URL(request.url).pathname;
+	const matches = endpoint.match(routeRegex)?.filter((x) => x != undefined);
 
-	switch (endpoint) {
-		case contactsRoute:
-			return routing.contacts();
-		default:
-			return routing.default();
+	if (!matches) {
+		return routing.default();
+	} else if (matches.length <= 3) {
+		return routing.contacts();
+	} else {
+		return routing.default();
 	}
 }
 
-function extractEndpointFromRequest(request: Request) {
-	let endpoint = new URL(request.url).pathname;
-
-	if (!endpoint.endsWith('/')) {
-		endpoint = `${endpoint}/`;
-	}
-
-	return endpoint;
+export function buildRouteRegex(
+	collections: string[]
+): RegExp {
+	return new RegExp(`^/${collections.map((x, idx) => `(${x})${idx > 0 ? '?' : ''}/?([^/\n]*)?/?`).join('')}$`);
 }
