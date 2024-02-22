680a00b3b98fc033607437d4e45ea4e90d8a8083
RuteSantos <1160663@isep.ipp.pt>
2024-02-22 18:22:37 +0000

    refactor: change the order of the defined classes in the response.ts file

--- src/http/response.ts
+++ src/http/response.ts
@@ -1,11 +1,3 @@
-export class CORSResponse extends Response {
-	constructor(protected readonly response: Response) {
-		super(response.body, response);
-		this.headers.set('Access-Control-Allow-Origin', '*');
-		this.headers.append('Vary', 'Origin');
-	}
-}
-
 export abstract class ErrorResponse extends Response {
 	constructor(protected readonly message: string | undefined, status: number) {
 		super(message ? JSON.stringify({ message: message }) : undefined, {
@@ -17,5 +9,13 @@ export abstract class ErrorResponse extends Response {
 export class NotFoundError extends ErrorResponse {
 	constructor() {
 		super(undefined, 404);
+	}
+}
+
+export class CORSResponse extends Response {
+	constructor(protected readonly response: Response) {
+		super(response.body, response);
+		this.headers.set('Access-Control-Allow-Origin', '*');
+		this.headers.append('Vary', 'Origin');
 	}
 }
